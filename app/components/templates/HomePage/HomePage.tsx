7fbf2b33d1ddf82f8746e0237981ddaa00a85143
Yannick Wittwer <wittwer.yannick@gmx.ch>
2021-04-26 18:16:42 +0200

    add mailchimp api

--- app/components/templates/HomePage/HomePage.tsx
+++ app/components/templates/HomePage/HomePage.tsx
@@ -1,10 +1,5 @@
 import styles from './HomePage.module.scss';
-import Counter from '@module/Counter/Counter';
 
-const HomePage = () => (
-  <div className={styles.content}>
-    <Counter />
-  </div>
-);
+const HomePage = () => <div className={styles.content}></div>;
 
 export default HomePage;
